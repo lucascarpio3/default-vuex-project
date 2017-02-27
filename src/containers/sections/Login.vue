02f026a41b8a23f804f5c8d1e36df68b62411a9c
Lucas Cárpio <lucas.carpio3@gmail.com>
2017-02-27 12:25:53 -0300

    login

--- src/containers/sections/Login.vue
+++ src/containers/sections/Login.vue
@@ -1,10 +1,13 @@
 <template>
-  <div>
-    Login
-    <div class="form-login">
-      <input type="text">
-      <input type="password">
-      <input type="button" value="Login">
+  <div class="login center">
+    <img src="../../assets/images/ipiranga_logo.png" class="login-logo mt-90" alt="Ipiranga logomarca"> <br>
+    <div class="login-form border--white--1 pd-50 br-15 mt-90 center">
+      <span class="typo-title--15 block left">Adicione seu usuário e senha para acessar.</span>
+      <label for="username" class="mt-40 block left">Usuário</label>
+      <input id="username" name="username" type="text" class="block full-width" autocomplete="off">
+      <label for="password" class="mt-40 block left">Senha</label>
+      <input id="password" type="password" class="block full-width" autocomplete="off">
+      <input type="button" value="Entrar" class="bkg-white btn br-15 login-form-submit mt-40 btn--flat pd-15 btn-lg">
     </div>
   </div>
 
