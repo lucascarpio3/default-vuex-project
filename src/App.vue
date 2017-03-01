028e71a3e9427bda81347f021e55f4f9dad99bad
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-01 15:40:55 -0300

    include menu and topbar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
     <top-bar></top-bar>
+    <div class="container">
+    <faq-menu></faq-menu>
+    <div class="section">
     <router-view></router-view>
-    <footer></footer>
+    </div>
+  </div>
   </div>
 </template>
 
 <script>
   import TopBar from 'containers/TopBar.vue'
-  import Footer from 'containers/Footer.vue'
+  import FaqMenu from 'containers/Menu.vue'
   export default {
     name: 'app',
     components: {
       TopBar,
-      Footer
+      FaqMenu
     }
   }
 </script>
--- src/containers/TopBar.vue
+++ src/containers/TopBar.vue
@@ -1,17 +1,13 @@
 <template>
-    <div class="app-wrapper" id="root">
-        <div class="help-container">
-            <div class="topbar hidden-sm hidden-xs">
-                <div class="container">
-                    <ul class="topbar-links row">
-                        <li class="nav-item col-sm-1-6 inline-block block-middle float-none"><a href="http://www.ipiranga.com.br/" target="_blank"><img src="/static/img/logo.ec595b6.jpg" alt="Ipiranga Confraria"></a></li>
-                        <li class="nav-item col-sm-1-6 inline-block block-middle float-none"><a href="https://www.kmdevantagens.com.br/wps/portal/kmv" target="_blank">Km de Vantagens</a></li>
-                        <li class="nav-item col-sm-1-6 inline-block block-middle float-none"><a href="https://www.postoipiranganaweb.com.br" target="_blank">Posto Ipiranga na Web</a></li>
-                        <li class="nav-item col-sm-1-6 inline-block block-middle float-none"><a href="http://www.conectcar.com/" target="_blank">Conectcar</a></li>
-                        <li class="nav-item col-sm-1-6 inline-block block-middle float-none"><a href="https://www.ipirangafrotas.com.br" target="_blank">Ipiranga Frotas</a></li>
-                        <li class="nav-item col-sm-1-6 inline-block block-middle float-none"><a href="http://www.ipiranga.com.br/wps/portal/ipiranga/cartoesipiranga/cartaoipiranga" target="_blank">Cartão Ipiranga</a></li>
-                    </ul>
-                </div>
+    <div class="top-bar">
+        <div class="top-bar-left">
+            <img class="logo" src="/static/img/logo-ipiranga.png" alt="Ipiranga Confraria">
+        </div>
+        <div class="top-bar-right">
+            <h1 class="faq ml-30">FAQ</h1>
+            <div class="logout">LOGOUT</div>
+            <div class="sign-out">
+                <i class="fa fa-sign-out" aria-hidden="true"></i>
             </div>
         </div>
     </div>
