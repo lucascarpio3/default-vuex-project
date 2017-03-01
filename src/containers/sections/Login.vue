985f88bbd41039c9bf0abf3a7505a89daed22c3f
Lucas Cárpio <lucas.carpio3@gmail.com>
2017-03-01 17:58:54 -0300

    login completo

--- src/containers/sections/Login.vue
+++ src/containers/sections/Login.vue
@@ -3,14 +3,15 @@
     <img src="../../assets/images/ipiranga_logo.png" class="login-logo mt-90" alt="Ipiranga logomarca"> <br>
     <form class="login-form border--white--1 pd-50 br-15 mt-90 center" @submit="signin">
       <span class="typo-title--15 block left">Adicione seu usuário e senha para acessar.</span>
-      <label for="username" class="mt-40 block left">Usuário</label>
-      <input v-model="credentials.username" @change="validateField" id="username" name="username" type="text"
-             class="block full-width mt-2">
-      <small v-if="">{{errors.username[0]}}</small>
-      <label for="password" class="mt-40 block left">Senha</label>
-      <input v-model="credentials.password" name="password" @change="validateField" id="password" type="password"
-             class="block full-width mt-2">
-      <small v-if="errors.password">{{errors.password[0]}}</small>
+      <label for="username" :class="['mt-40', 'block', 'left', {error: errors.username}]">Usuário</label>
+      <input v-model="credentials.username" @blur="validateField" id="username" name="username" type="text"
+             :class="['block', 'full-width', 'mt-2', {error: errors.username}]">
+      <small class="c-carnation" v-if="errors.username">{{errors.username[0]}}</small>
+      <label for="password" :class="['mt-40', 'block', 'left', {error: errors.password}]">Senha</label>
+      <input v-model="credentials.password" name="password" @blur="validateField" id="password" type="password"
+             :class="['block', 'full-width', 'mt-2', {error: errors.password}]">
+      <small class="c-carnation" v-if="errors.password">{{errors.password[0]}}</small>
+      <br>
       <input type="submit" value="ENTRAR" class="bkg-white btn br-15 login-form-submit mt-40 btn--flat pd-15 btn-lg">
     </form>
     <div class="login-bkg"></div>
