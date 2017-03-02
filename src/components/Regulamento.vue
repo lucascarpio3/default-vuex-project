01e689e3e1731261f91e6a673d743374ba0fc819
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-02 14:44:24 -0300

    section regulamento

--- src/components/Regulamento.vue
+++ src/components/Regulamento.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="regulation">
-
+    <button class="bkg-maroon-flush btn mt-40 btn--flat pd-18 btn-default br-15 c-white">UPLOAD</button>
+    <label class="tam-max block mv-5 c-pewter">* Tamanho máximo de 5mb. Formato pdf.</label>
+    <iframe class="pdf-shower block" src="https://www.escrevendoofuturo.org.br/arquivos/5626/olp216-regulamento-24fev2016-ok.pdf" width="800px" height="580px" style="border: none;"></iframe>
+    <label class="regulation-name c-pewter">nomedoarquivo.pdf</label>
   </div>
 </template>
 
