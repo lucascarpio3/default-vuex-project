91d4564bf8df769d551e8c398f8d3214454a1ec2
Felipe de Aquino Nascimento <feelipe.an@gmail.com>
2017-03-02 16:58:15 -0300

    finalizando menu

--- src/containers/Menu.vue
+++ src/containers/Menu.vue
@@ -1,9 +1,9 @@
 <template>
-    <aside class="menu">
-        <div class="imagens"></div>
-        <div class="planos-frete"></div>
-        <div class="faq"></div>
-        <div class="regulamento"></div>
+    <aside class="menu pl-70">
+        <h1 class="imagens pt-38 c-white"><i class="fa fa-picture-o" aria-hidden="true"></i> IMAGENS</h1>
+        <h1 class="planos-frete c-white"><img src="../assets/images/shape.png"> PLANOS / FRETE</h1>
+        <h1 class="faq c-white"><i class="fa fa-exclamation-triangle" aria-hidden="true"></i>FAQ</h1>
+        <h1 class="regulamento c-white"><i class="fa fa-file-text-o" aria-hidden="true"></i>REGULAMENTO</h1>
     </aside>
 </template>
 
