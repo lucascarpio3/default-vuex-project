dce366787b9afcd17d42641e6afcef0e78967525
Lucas Cárpio <lucas.carpio3@gmail.com>
2017-03-02 15:14:08 -0300

    ajustes finos

--- src/containers/TopBar.vue
+++ src/containers/TopBar.vue
@@ -1,25 +1,25 @@
 <template>
-    <div class="top-bar">
-        <div class="top-bar-left">
-            <img class="logo" src="../assets/images/ipiranga_logo.png" alt="Ipiranga Confraria">
-        </div>
-        <div class="top-bar-right">
-            <h1 class="faq ml-30">{{titlePage}}</h1>
-            <div class="logout ml-auto mr-156">LOGOUT</div>
-            <div class="sign-out">
-                <i class="fa fa-sign-out" aria-hidden="true"></i>
-            </div>
-        </div>
+  <div class="top-bar">
+    <div class="top-bar-left">
+      <img class="logo" src="../assets/images/ipiranga_logo.png" alt="Ipiranga Confraria">
     </div>
+    <div class="top-bar-right">
+      <h1 class="faq ml-30">{{titlePage}}</h1>
+      <div class="logout ml-auto mr-156">
+        LOGOUT
+        <i class="fa fa-sign-out" aria-hidden="true"></i>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-   export default {
-     props: {
-       titlePage: {
-         type: String,
-         required: true
-       }
-     }
-   }
+  export default {
+    props: {
+      titlePage: {
+        type: String,
+        required: true
+      }
+    }
+  }
 </script>
