7b0095d5ca298cf02a9fee4ff9c73d64bb6cd1f3
Felipe de Aquino Nascimento <feelipe.an@gmail.com>
2017-03-03 16:29:36 -0300

    screen plans-freight

--- src/components/PlanosFrete.vue
+++ src/components/PlanosFrete.vue
@@ -1,6 +1,24 @@
 <template>
     <div class="plan-freight">
+        <div class="tabs-bar mt-40">
+        <div :class="['tabs-bar-item', {'active': activated === 'planos'}]" @click="select('planos')">PLANOS</div>
+        <div :class="['tabs-bar-item', {'active': activated === 'frete'}]" @click="select('frete')">FRETE MENSAL</div>
+        </div>
     </div>
 </template>
 
-<script></script>
+<script>
+    export default {
+      data () {
+        return {
+          activated: 'planos'
+
+        }
+      },
+      methods: {
+        select (selected) {
+          this.activated = selected
+        }
+      }
+    }
+</script>
