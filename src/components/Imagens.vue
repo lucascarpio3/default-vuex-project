c3e6612d3a10fb3c6aefe7983db36fd7ad820c2e
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-06 18:56:35 -0300

    screen images

--- src/components/Imagens.vue
+++ src/components/Imagens.vue
@@ -1,5 +1,29 @@
 <template>
     <div class="images">
+          <div class="section-image-1">
+        <div class="logo-site">Logo Site <br>
+            <img src="/../../assets/images/logo_confraria-do-vinho"></div>
+        <div class="logo-kmV">Logo KmV <br>
+            <img src="/../../assets/images/kmv.png"></div>
+        <div class="duas-garrafas">2 Garrafas <br>
+            <img src="/../../assets/images/2garrafas.png"></div>
+        <div class="quatro-garrafas">4 Garrafas <br>
+            <img src="/../../assets/images/4garrafas.png"></div>
+        <div class="seis-garrafas">6 Garrafas <br>
+            <img src="/../../assets/images/6garrafas.png"></div>
+          </div>
+
+        <div class="section-image-2">
+            <div class="explorador">Explorador <br>
+                <img src="/../../assets/images/vinhobasico1.png">
+            </div>
+            <div class="selecao">Seleção <br>
+                <img src="/../../assets/images/vinhobasico1.png">
+            </div>
+            <div class="joias">Jóias <br>
+                <img src="/../../assets/images/vinhobasico1.png">
+            </div>
+        </div>
     </div>
 </template>
 
