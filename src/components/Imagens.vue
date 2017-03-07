f7c9ef8bec2546e8e41f567c783a4eb2dcc599cd
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-07 17:19:08 -0300

    ajustes 2

--- src/components/Imagens.vue
+++ src/components/Imagens.vue
@@ -1,43 +1,39 @@
 <template>
     <div>
-    <div class="row-plans">
-        <div class="images col-md-12">
-            <section class="plans-delivery">
-                <div class="logo-site c-maroon-flush mr-20">Logo Site <br><br> <img src="../assets/images/logo_confraria-do-vinho.png">
-                    <div class="label-logo c-black">confrariadovinho.png</div>
-                </div>
-                <div class="logo-kmV c-maroon-flush mr-20">Logo KmV <br><br> <img src="../assets/images/kmv.png">
-                    <div class="label-kmV c-black">kmv.png</div>
-                </div>
-                <div class="duas-garrafas c-maroon-flush mr-20">2 Garrafas <br><br> <img src="../assets/images/2garrafas.png">
-                    <div class="label-2 c-black">2garrafas.png</div>
-                </div>
-                <div class="quatro-garrafas c-maroon-flush mr-20">4 Garrafas <br><br> <img src="../assets/images/4garrafas.png">
-                    <div class="label-4 c-black">4garrafas.png</div>
-                </div>
-                <div class="seis-garrafas c-maroon-flush mr-20">6 Garrafas <br><br> <img src="../assets/images/6garrafas.png">
-                    <div class="label-6 c-black">6garrafas.png</div>
-                </div>
-            </section>
-          </div>
+        <div class="row-plans">
+            <div class="images col-md-12">
+                <section class="plans-delivery">
+                    <div class="logo-site c-maroon-flush mr-20">Logo Site <br><br> <img src="../assets/images/logo_confraria-do-vinho.png">
+                        <div class="label-logo c-black">confrariadovinho.png</div>
+                    </div>
+                    <div class="logo-kmV c-maroon-flush mr-20">Logo KmV <br><br> <img src="../assets/images/kmv.png">
+                        <div class="label-kmV c-black">kmv.png</div>
+                    </div>
+                    <div class="duas-garrafas c-maroon-flush mr-20">2 Garrafas <br><br> <img src="../assets/images/2garrafas.png">
+                        <div class="label-2 c-black">2garrafas.png</div>
+                    </div>
+                    <div class="quatro-garrafas c-maroon-flush mr-20">4 Garrafas <br><br> <img src="../assets/images/4garrafas.png">
+                        <div class="label-4 c-black">4garrafas.png</div>
+                    </div>
+                    <div class="seis-garrafas c-maroon-flush mr-20">6 Garrafas <br><br> <img src="../assets/images/6garrafas.png">
+                        <div class="label-6 c-black">6garrafas.png</div>
+                    </div>
+                </section>
+            </div>
             <label class="tam-max c-pewter pl-30">*Tamanho máximo de 2mb. Formato png ou jpg.</label>
-    </div>
-    <div class="row-images">
-        <div class="images-details">
-           <section class="product-details">
-        <div class="explorador c-maroon-flush mr-20 pl-30">Explorador <br><br> <img src="../assets/images/vinhobasico1.jpg">
-            <div class="label-logo c-black">vinhobasico1.jpg</div>
         </div>
-        <div class="selecao c-maroon-flush mr-20 pl-30">Seleção <br><br> <img src="../assets/images/vinhobasico2.jpg">
-            <div class="label-logo c-black">vinhobasico2.jpg</div>
-        </div>
-        <div class="joias c-maroon-flush mr-20 pl-30">Jóias <br><br> <img src="../assets/images/vinhobasico3.jpg">
-            <div class="label-logo c-black">vinhobasico3.jpg</div>
-        </div>
-           </section>
+        <div class="row-images mt-50">
+            <wine-card></wine-card>
         </div>
     </div>
     </div>
 </template>
 
-<script></script>
+<script>
+  import WineCard from 'components/WineCard'
+  export default {
+    components: {
+      WineCard
+    }
+  }
+</script>
---
+++ src/components/WineCard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="wine-card">
+        <div class="explorador c-maroon-flush mr-20 pl-30">Explorador <br><br><img src="../assets/images/vinhobasico1.jpg">
+            <div class="label-logo c-black">vinhobasico1.jpg</div>
+        </div>
+        <div class="selecao c-maroon-flush mr-20 pl-30">Seleção  <br><br><img src="../assets/images/vinhobasico2.jpg">
+            <div class="label-logo c-black">vinhobasico2.jpg</div>
+        </div>
+        <div class="joias c-maroon-flush mr-20 pl-30">Jóias <br><br> <img src="../assets/images/vinhobasico3.jpg">
+            <div class="label-logo c-black">vinhobasico3.jpg</div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
