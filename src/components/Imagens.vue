9ad3d2280b811cc0692c4343b922fa771670f5f7
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-07 12:00:21 -0300

    Imagens 50%

--- src/components/Imagens.vue
+++ src/components/Imagens.vue
@@ -1,29 +1,25 @@
 <template>
-    <div class="images">
-          <section class="plans-delivery">
-        <div class="logo-site">Logo Site <br>
-            <img src="/../../assets/images/logo_confraria-do-vinho"></div>
-        <div class="logo-kmV">Logo KmV <br>
-            <img src="/../../assets/images/kmv.png"></div>
-        <div class="duas-garrafas">2 Garrafas <br>
-            <img src="/../../assets/images/2garrafas.png"></div>
-        <div class="quatro-garrafas">4 Garrafas <br>
-            <img src="/../../assets/images/4garrafas.png"></div>
-        <div class="seis-garrafas">6 Garrafas <br>
-            <img src="/../../assets/images/6garrafas.png"></div>
-          </section>
-
-        <section class="image-products">
-            <div class="explorador">Explorador <br>
-                <img src="/../../assets/images/vinhobasico1.png">
-            </div>
-            <div class="selecao">Seleção <br>
-                <img src="/../../assets/images/vinhobasico1.png">
-            </div>
-            <div class="joias">Jóias <br>
-                <img src="/../../assets/images/vinhobasico1.png">
-            </div>
-        </section>
+    <div class="row">
+        <div class="images col-md-12">
+            <section class="plans-delivery">
+                <div class="logo-site c-maroon-flush">Logo Site <br> <img src="/../../assets/images/logo_confraria-do-vinho">
+                    <div class="label-logo c-black">confrariadovinho.png</div>
+                </div>
+                <div class="logo-kmV c-maroon-flush">Logo KmV <br> <img src="/../../assets/images/kmv.png">
+                    <div class="label-kmV c-black">kmv.png</div>
+                </div>
+                <div class="duas-garrafas c-maroon-flush">2 Garrafas <br> <img src="/../../assets/images/2garrafas.png">
+                    <div class="label-2 c-black">2garrafas.png</div>
+                </div>
+                <div class="quatro-garrafas c-maroon-flush">4 Garrafas <br> <img src="/../../assets/images/4garrafas.png">
+                    <div class="label-4 c-black">4garrafas.png</div>
+                </div>
+                <div class="seis-garrafas c-maroon-flush">6 Garrafas <br> <img src="/../../assets/images/6garrafas.png">
+                    <div class="label-6 c-black">6garrafas.png</div>
+                </div>
+            </section>
+          </div>
+        <label class="tam-max c-pewter">*Tamanho máximo de 2mb. Formato png ou jpg.</label>
     </div>
 </template>
 
