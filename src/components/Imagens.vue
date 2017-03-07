346c0417afb73e09e2bc4e5b25a5ae9b247ad7d8
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-07 12:25:04 -0300

    Ajustes tela Imagens

--- src/components/Imagens.vue
+++ src/components/Imagens.vue
@@ -1,20 +1,20 @@
 <template>
-    <div class="row">
+    <div class="row" style="width: 100%; height: 50%;">
         <div class="images col-md-12">
             <section class="plans-delivery">
-                <div class="logo-site c-maroon-flush">Logo Site <br> <img src="/../../assets/images/logo_confraria-do-vinho">
+                <div class="logo-site c-maroon-flush mr-20">Logo Site <br><br> <img src="../assets/images/logo_confraria-do-vinho.png">
                     <div class="label-logo c-black">confrariadovinho.png</div>
                 </div>
-                <div class="logo-kmV c-maroon-flush">Logo KmV <br> <img src="/../../assets/images/kmv.png">
+                <div class="logo-kmV c-maroon-flush mr-20">Logo KmV ><br><br> <img src="../assets/images/kmv.png">
                     <div class="label-kmV c-black">kmv.png</div>
                 </div>
-                <div class="duas-garrafas c-maroon-flush">2 Garrafas <br> <img src="/../../assets/images/2garrafas.png">
+                <div class="duas-garrafas c-maroon-flush mr-20">2 Garrafas <br><br> <img src="../assets/images/2garrafas.png">
                     <div class="label-2 c-black">2garrafas.png</div>
                 </div>
-                <div class="quatro-garrafas c-maroon-flush">4 Garrafas <br> <img src="/../../assets/images/4garrafas.png">
+                <div class="quatro-garrafas c-maroon-flush mr-20">4 Garrafas <br><br> <img src="../assets/images/4garrafas.png">
                     <div class="label-4 c-black">4garrafas.png</div>
                 </div>
-                <div class="seis-garrafas c-maroon-flush">6 Garrafas <br> <img src="/../../assets/images/6garrafas.png">
+                <div class="seis-garrafas c-maroon-flush mr-20">6 Garrafas <br><br> <img src="../assets/images/6garrafas.png">
                     <div class="label-6 c-black">6garrafas.png</div>
                 </div>
             </section>
