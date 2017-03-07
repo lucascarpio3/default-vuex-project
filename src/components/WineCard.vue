3104bbb747f54b5d66a0dd2a4bf238c762617044
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-07 17:56:38 -0300

    imagens 60%

--- src/components/WineCard.vue
+++ src/components/WineCard.vue
@@ -1,13 +1,22 @@
 <template>
     <div class="wine-card">
-        <div class="explorador c-maroon-flush mr-20 pl-30">Explorador <br><br><img src="../assets/images/vinhobasico1.jpg">
-            <div class="label-logo c-black">vinhobasico1.jpg</div>
+        <div class="explorador c-maroon-flush mr-20 pl-30">Explorador <br><br>
+            <div class="bg-white">
+            <img src="../assets/images/vinhobasico1.jpg">
+            </div>
+            <div class="label-logo c-black pt-10">vinhobasico1.jpg</div>
         </div>
-        <div class="selecao c-maroon-flush mr-20 pl-30">Seleção  <br><br><img src="../assets/images/vinhobasico2.jpg">
-            <div class="label-logo c-black">vinhobasico2.jpg</div>
+        <div class="selecao c-maroon-flush mr-20 pl-30">Seleção  <br><br>
+            <div class="bg-white">
+            <img src="../assets/images/vinhobasico2.jpg">
+            </div>
+            <div class="label-logo c-black pt-10">vinhobasico2.jpg</div>
         </div>
-        <div class="joias c-maroon-flush mr-20 pl-30">Jóias <br><br> <img src="../assets/images/vinhobasico3.jpg">
-            <div class="label-logo c-black">vinhobasico3.jpg</div>
+        <div class="joias c-maroon-flush mr-20 pl-30">Jóias <br><br>
+            <div class="bg-white">
+            <img src="../assets/images/vinhobasico3.jpg">
+            </div>
+            <div class="label-logo c-black pt-10">vinhobasico3.jpg</div>
         </div>
     </div>
 </template>
