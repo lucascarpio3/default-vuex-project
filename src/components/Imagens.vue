89be8d944b7be9ae2ff058313d47fe92e2c3d577
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-07 16:15:58 -0300

    images

--- src/components/Imagens.vue
+++ src/components/Imagens.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="row" style="width: 100%; height: 50%;">
+    <div>
+    <div class="row-plans">
         <div class="images col-md-12">
             <section class="plans-delivery">
                 <div class="logo-site c-maroon-flush mr-20">Logo Site <br><br> <img src="../assets/images/logo_confraria-do-vinho.png">
                     <div class="label-logo c-black">confrariadovinho.png</div>
                 </div>
-                <div class="logo-kmV c-maroon-flush mr-20">Logo KmV ><br><br> <img src="../assets/images/kmv.png">
+                <div class="logo-kmV c-maroon-flush mr-20">Logo KmV <br><br> <img src="../assets/images/kmv.png">
                     <div class="label-kmV c-black">kmv.png</div>
                 </div>
                 <div class="duas-garrafas c-maroon-flush mr-20">2 Garrafas <br><br> <img src="../assets/images/2garrafas.png">
@@ -19,7 +20,23 @@
                 </div>
             </section>
           </div>
-        <label class="tam-max c-pewter">*Tamanho máximo de 2mb. Formato png ou jpg.</label>
+            <label class="tam-max c-pewter pl-30">*Tamanho máximo de 2mb. Formato png ou jpg.</label>
+    </div>
+    <div class="row-images">
+        <div class="images-details">
+           <section class="product-details">
+        <div class="explorador c-maroon-flush mr-20 pl-30">Explorador <br><br> <img src="../assets/images/vinhobasico1.jpg">
+            <div class="label-logo c-black">vinhobasico1.jpg</div>
+        </div>
+        <div class="selecao c-maroon-flush mr-20 pl-30">Seleção <br><br> <img src="../assets/images/vinhobasico2.jpg">
+            <div class="label-logo c-black">vinhobasico2.jpg</div>
+        </div>
+        <div class="joias c-maroon-flush mr-20 pl-30">Jóias <br><br> <img src="../assets/images/vinhobasico3.jpg">
+            <div class="label-logo c-black">vinhobasico3.jpg</div>
+        </div>
+           </section>
+        </div>
+    </div>
     </div>
 </template>
 
