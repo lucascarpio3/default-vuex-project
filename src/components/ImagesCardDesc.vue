020c996ab1b5b01bab73226567487eb1792f5527
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-08 18:06:11 -0300

    ajustes tela imagens

--- src/components/ImagesCardDesc.vue
+++ src/components/ImagesCardDesc.vue
@@ -4,7 +4,7 @@
         <div class="images col-md-12">
             <section class="plans-delivery">
                 <div class="logo-site c-maroon-flush mr-20">Logo Site <br><br>
-                    <div class="bg-white">
+                    <div class="card-white">
                         <img src="../assets/images/logo_confraria-do-vinho.png">
                     </div>
                     <div class="label-logo c-black">confrariadovinho.png</div>
@@ -16,7 +16,7 @@
                     </div>
                 </div>
                 <div class="logo-kmV c-maroon-flush mr-20">Logo KmV <br><br>
-                    <div class="bg-white">
+                    <div class="card-white">
                         <img src="../assets/images/kmv.png">
                     </div>
                     <div class="label-kmV c-black">kmv.png</div>
@@ -28,7 +28,7 @@
                     </div>
                 </div>
                 <div class="duas-garrafas c-maroon-flush mr-20">2 Garrafas <br><br>
-                    <div class="bg-white">
+                    <div class="card-white">
                         <img src="../assets/images/2garrafas.png">
                     </div>
                     <div class="label-2 c-black">2garrafas.png</div>
@@ -40,7 +40,7 @@
                     </div>
                 </div>
                 <div class="quatro-garrafas c-maroon-flush mr-20">4 Garrafas <br><br>
-                    <div class="bg-white">
+                    <div class="card-white">
                         <img src="../assets/images/4garrafas.png">
                     </div>
                     <div class="label-4 c-black">4garrafas.png</div>
@@ -52,7 +52,7 @@
                     </div>
                 </div>
                 <div class="seis-garrafas c-maroon-flush mr-20">6 Garrafas <br><br>
-                    <div class="bg-white">
+                    <div class="card-white">
                         <img src="../assets/images/6garrafas.png">
                     </div>
                     <div class="label-6 c-black">6garrafas.png</div>
--- src/components/WineCard.vue
+++ src/components/WineCard.vue
@@ -2,7 +2,7 @@
     <div>
     <div class="wine-card">
         <div class="explorador c-maroon-flush mr-20 pl-30">Explorador <br><br>
-            <div class="bg-white">
+            <div class="card-white-wine">
                 <img src="../assets/images/vinhobasico1.jpg">
             </div>
             <div class="label-logo c-black pt-10">vinhobasico1.jpg</div>
@@ -16,7 +16,7 @@
             </div>
         </div>
         <div class="selecao c-maroon-flush mr-20 pl-30">Seleção  <br><br>
-            <div class="bg-white">
+            <div class="card-white-wine">
                 <img src="../assets/images/vinhobasico2.jpg">
             </div>
             <div class="label-logo c-black pt-10">vinhobasico2.jpg</div>
@@ -30,7 +30,7 @@
             </div>
         </div>
         <div class="joias c-maroon-flush mr-20 pl-30">Jóias <br><br>
-            <div class="bg-white">
+            <div class="card-white-wine">
                 <img src="../assets/images/vinhobasico3.jpg">
             </div>
             <div class="label-logo c-black pt-10">vinhobasico3.jpg</div>
