0e6982da95f4384c55bf95dbdb505ed51d35a4ab
Bruno Barbosa Silva <bruno.barbosa@zup.com.br>
2017-03-09 11:57:02 -0300

    Ajustes:
    # Tela de Imagens
    # Tela de Login

--- src/components/ImagesCard.vue
+++ src/components/ImagesCard.vue
@@ -1,43 +1,45 @@
 <template>
     <div>
-    <div class="row-plans">
-        <div class="images col-md-12">
-            <section class="plans-delivery">
-                <div class="logo-site c-maroon-flush mr-20">Logo Site <br><br>
-                    <div class="card-white">
-                        <img src="../assets/images/logo_confraria-do-vinho.png">
+        <div class="mb-40">
+            <div class="images col-md-12">
+                <section class="plans-delivery">
+                    <div class="logo-site c-maroon-flush mr-20 fw-500">Logo Site <br><br>
+                        <div class="card-white">
+                            <img src="../assets/images/logo_confraria-do-vinho.png">
+                        </div>
+                        <div class="label-logo c-black">confrariadovinho.png</div>
                     </div>
-                    <div class="label-logo c-black">confrariadovinho.png</div>
-                </div>
-                <div class="logo-kmV c-maroon-flush mr-20">Logo KmV <br><br>
-                    <div class="card-white">
-                        <img src="../assets/images/kmv.png">
+                    <div class="logo-kmV c-maroon-flush mr-20">Logo KmV <br><br>
+                        <div class="card-white">
+                            <img src="../assets/images/kmv.png">
+                        </div>
+                        <div class="label-kmV c-black">kmv.png</div>
                     </div>
-                    <div class="label-kmV c-black">kmv.png</div>
-                </div>
-                <div class="duas-garrafas c-maroon-flush mr-20">2 Garrafas <br><br>
-                    <div class="card-white">
-                        <img src="../assets/images/2garrafas.png">
+                    <div class="duas-garrafas c-maroon-flush mr-20 fw-500">2 Garrafas <br><br>
+                        <div class="card-white">
+                            <img src="../assets/images/2garrafas.png">
+                        </div>
+                        <div class="label-2 c-black">2garrafas.png</div>
                     </div>
-                    <div class="label-2 c-black">2garrafas.png</div>
-                </div>
-                <div class="quatro-garrafas c-maroon-flush mr-20">4 Garrafas <br><br>
-                    <div class="card-white">
-                        <img src="../assets/images/4garrafas.png">
+                    <div class="quatro-garrafas c-maroon-flush mr-20 fw-500">4 Garrafas <br><br>
+                        <div class="card-white">
+                            <img src="../assets/images/4garrafas.png">
+                        </div>
+                        <div class="label-4 c-black">4garrafas.png</div>
                     </div>
-                    <div class="label-4 c-black">4garrafas.png</div>
-                </div>
-                <div class="seis-garrafas c-maroon-flush mr-20">6 Garrafas <br><br>
-                    <div class="card-white">
-                        <img src="../assets/images/6garrafas.png">
+                    <div class="seis-garrafas c-maroon-flush mr-20 fw-500">6 Garrafas <br><br>
+                        <div class="card-white">
+                            <img src="../assets/images/6garrafas.png">
+                        </div>
+                        <div class="label-6 c-black">6garrafas.png</div>
                     </div>
-                    <div class="label-6 c-black">6garrafas.png</div>
-                </div>
-            </section>
+                </section>
+            </div>
+            <label class="tam-max c-pewter pl-30">*Tamanho máximo de 2mb. Formato png ou jpg.</label>
+            <button class="bkg-maroon-flush btn btn--flat pd-18 btn-default br-15 c-white psr-100 position--absolute">
+                SALVAR
+            </button>
         </div>
-        <label class="tam-max c-pewter pl-30">*Tamanho máximo de 2mb. Formato png ou jpg.</label>
-    </div>
-    <button class="bkg-maroon-flush btn mt-40 btn--flat pd-18 btn-default br-15 c-white">SALVAR</button>
     </div>
 </template>
 
