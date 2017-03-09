c796cafed51e8699bd3c5b9b6b9b09d8ce5ef07a
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-09 12:00:48 -0300

    screen plans-freight fixes

---
+++ src/components/PlanTabFreight.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="row-plans-freight">
+        <div class="columns-plans ml-120 mr-30">
+        <p>Plano 1</p>
+        <input>
+            <p>Descrição</p>
+            <textarea name="" id="" cols="20" rows="3"></textarea>
+              <p>Regras</p>
+            <p>Quantidade de Km</p>
+            <textarea name="" id="" cols="10" rows="1"></textarea>
+            <p>Sem troca de Km + %</p>
+            <textarea name="" id="" cols="10" rows="1"></textarea>
+        </div>
+        <div class="columns-plans mr-30">
+            <p>Plano 2</p>
+            <input>
+            <p>Descrição</p>
+            <textarea name="" id="" cols="20" rows="3"></textarea>
+            <p>Regras</p>
+            <p>Quantidade de Km</p>
+            <textarea name="" id="" cols="10" rows="1"></textarea>
+            <p>Sem troca de Km + %</p>
+            <textarea name="" id="" cols="10" rows="1"></textarea>
+        </div>
+        <div class="columns-plans mr-30">
+            <p>Plano 3</p>
+            <input>
+            <p>Descrição</p>
+            <textarea name="" id="" cols="20" rows="3"></textarea>
+            <p>Regras</p>
+            <p>Quantidade de Km</p>
+            <textarea name="" id="" cols="10" rows="1"></textarea>
+            <p>Sem troca de Km + %</p>
+            <textarea name="" id="" cols="10" rows="1"></textarea>
+        </div>
+    </div>
+</template>
+
+<script></script>
--- src/containers/sections/PlanosFrete.vue
+++ src/containers/sections/PlanosFrete.vue
@@ -1,16 +1,24 @@
 <template>
     <!--Planos-->
     <div class="plan-freight">
-        <div class="tabs-bar mt-40">
-        <div :class="['tabs-bar-item', {'active': activated === 'planos'}]" @click="select('planos')">PLANOS</div>
+        <div class="tabs-bar mt-40 pl-50">
+        <div :class="['tabs-bar-item', {'active': activated === 'planos'}]" @click="select('planos')">PLANOS
+        </div>
             <div :class="['tabs-bar-item', {'active': activated === 'personalizacao'}]" @click="select('personalizacao')">PERSONALIZAÇÃO DE RÓTULOS</div>
         <div :class="['tabs-bar-item', {'active': activated === 'frete'}]" @click="select('frete')">FRETE MENSAL</div>
+        </div>
+        <div class="row-section-plans">
+            <plan-tab-freight></plan-tab-freight>
         </div>
     </div>
 </template>
 
 <script>
+    import PlanTabFreight from 'components/PlanTabFreight'
     export default {
+      components: {
+        PlanTabFreight
+      },
       data () {
         return {
           activated: 'planos'
