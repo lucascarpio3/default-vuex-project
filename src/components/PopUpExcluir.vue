7d874b30026f16553e780bfdd2f6ff4640e36f28
Felipe de Aquino Nascimento <felipe.nascimento@zup.com.br>
2017-03-16 18:05:51 -0300

    Modal done and popups

---
+++ src/components/PopUpExcluir.vue
@@ -0,0 +1,41 @@
+<template type="text/x-template" id="popup-template">
+    <transition name="modal-popup">
+        <div class="modal-mask-popup">
+            <div class="modal-wrapper-popup">
+                <div class="modal-container-popup">
+                    <div class="modal-header-popup pl-30">
+                        <slot name="header-popup">
+                            Tem certeza que deseja excluir esse item?
+                        </slot>
+                    </div>
+                    <div class="modal-footer-popup pt-20">
+                        <slot name="footer-popup">
+                            <button class="btn btn--flat btn-default bkg-white c-maroon-flush br-15 mt-30" @click="closeModal"
+                                    style="border: 1px solid">
+                                CANCELAR
+                            </button>
+                            <button class="btn btn--flat btn-default bkg-shiraz c-white br-15 mt-30" @click="">
+                                EXCLUIR
+                            </button>
+                        </slot>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        showModal: false
+      }
+    },
+    methods: {
+      closeModal () {
+        this.$emit('closeModal')
+      }
+    }
+  }
+</script>
--- src/containers/sections/Ajuda.vue
+++ src/containers/sections/Ajuda.vue
@@ -7,24 +7,47 @@
       <card title="Resposta">
         <textarea class="help-answer" id="answer" rows="10"></textarea>
       </card>
-      <button class="btn btn--flat btn-default bkg-shiraz c-white br-15 mt-30">Adicionar</button>
+      <button @click="openModal()" class="btn btn--flat btn-default bkg-shiraz c-white br-15 mt-30">ADICIONAR</button>
+      <button @click="openModal()" class="btn btn--flat btn-default bkg-shiraz c-white br-15 mt-30">SALVAR</button>
     </div>
     <card title="Ajuda">
       <ul class="faq-questions">
         <faq-accordion v-for="item in items" :question="item" :key="item.id"></faq-accordion>
       </ul>
     </card>
+    <PopUpSalvar v-if="modalOpened" @closeModal="closeModal">
+    </PopUpSalvar>
+    <PopUpExcluir v-if="modalOpened" @closeModal="closeModal">
+    </PopUpExcluir>
   </section>
 </template>
 
 <script>
   import FaqAccordion from 'components/FaqAccordion'
   import Card from 'components/Card'
+  import PopUpSalvar from 'components/PopUpSalvar'
+  import PopUpExcluir from 'components/PopUpExcluir'
   import {mapState} from 'vuex'
   export default {
     components: {
       FaqAccordion,
-      Card
+      Card,
+      PopUpSalvar,
+      PopUpExcluir
+    },
+    data () {
+      return {
+        status: '',
+        modalOpened: false
+      }
+    },
+    methods: {
+      openModal () {
+        this.modalOpened = true
+      },
+      closeModal () {
+        this.modalOpened = false
+      }
     },
     computed: {
       ...mapState({
